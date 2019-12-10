271c9cbf8c952e0450d575af19b590ac13f51db7
agyen <izagyen96gmail.comgit config --global user.email izagyen96gmail.com>
2019-12-10 10:12:51 +0000

    populated database

--- stories.sql
+++ stories.sql
@@ -47,5 +47,31 @@ CREATE TABLE readers (
     reader_email varchar(50) not null,
     reader_password varchar(50) not null,
     current_story integer references stories(story_id) not null,
-    hapter_id integer references chapters(chapter_id)
+    chapter_id integer references chapters(chapter_id)
 );
+
+insert into categories("category_id", "category_name" ) values(701, "History");
+insert into categories("category_id", "category_name" ) values(702, "Fiction");
+insert into categories("category_id", "category_name" ) values(703, "Romance");
+insert into categories("category_id", "category_name" ) values(704, "Adventure");
+
+insert into stories("story_id", "story_title", "story_category", "story_photo") values(101, 'My choice', 703, 'www.photo.com');
+insert into stories("story_id", "story_title", "story_category", "story_photo") values(102, 'Destiny', 702, 'www.photo.com');
+insert into stories("story_id", "story_title", "story_category", "story_photo") values(103, 'Strength', 701, 'www.photo.com');
+insert into stories("story_id", "story_title", "story_category", "story_photo") values(104, 'The Arms', 704, 'www.photo.com');
+
+insert into chapters("chapter_id", "story_id", "chapter_title", "chapter_content") values(201, 101, 'There', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat');
+insert into chapters("chapter_id", "story_id", "chapter_title", "chapter_content") values(202, 101, 'Lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat');
+insert into chapters("chapter_id", "story_id", "chapter_title", "chapter_content") values(203, 102, 'Strike', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat');
+insert into chapters("chapter_id", "story_id", "chapter_title", "chapter_content") values(204, 102, 'Forbid', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat');
+
+insert into dependencies("dependency_id", "chapter_id", "content") values(401, 201, 'Go to chapter 2');
+insert into dependencies("dependency_id", "chapter_id", "content") values(402, 201, 'Go to chapter 1');
+insert into dependencies("dependency_id", "chapter_id", "content") values(403, 203, 'Go to chapter 02');
+insert into dependencies("dependency_id", "chapter_id", "content") values(404, 203, 'Go to chapter 01');
+
+insert into authors("author_id", "author_name", "story_id", "author_email", "author_password") values(501, 'Yaa Nuamah', 101, 'edna@email.com', '12435fgd');
+insert into authors("author_id", "author_name", "story_id", "author_email", "author_password") values(502, 'Isaac Agyen', 102, 'edna@email.com', '53f35fgd');
+
+insert into readers("reader_id", "reader_name", "reader_email", "reader_password", "chapter_id") values(601, 'Shadrack Ankomahene', 'shad@gmail.com', '1234gfv', 201);
+insert into readers("reader_id", "reader_name", "reader_email", "reader_password", "chapter_id") values(602, 'Sam Bartels', 'sam@gmail.com', '1234gfv', 203);
