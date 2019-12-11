846ba66d88db64427974b9e00776d29371584b16
ankomahene <ankomaheneshadrack@gmail.com>
2019-12-11 10:54:49 +0000

    update schema

--- stories.sql
+++ stories.sql
@@ -4,7 +4,8 @@ DROP TABLE if EXISTS chapters cascade;
 DROP TABLE if EXISTS dependencies cascade;
 DROP TABLE if EXISTS choices cascade;
 DROP TABLE if EXISTS authors cascade;
-DROP TABLE if EXISTS readers;
+DROP TABLE if EXISTS readers cascade;
+DROP TABLE if EXISTS session_info;
 
 CREATE TABLE categories (
     category_id integer primary key not null,
@@ -24,6 +25,7 @@ CREATE TABLE authors (
 CREATE TABLE stories (
     story_id integer primary key not null,
     story_title varchar(50) not null,
+    story_description text,
     category_id integer references categories(category_id),
     author_id integer references authors(author_id),
     story_photo text
@@ -54,9 +56,13 @@ CREATE TABLE choices (
 CREATE TABLE readers (
     reader_id integer primary key not null,
     reader_name varchar(50),
-    reader_email varchar(50) not null,
+    reader_email varchar(50) not null
+);
+
+CREATE TABLE session_info (
+    reader_id integer references readers(reader_id) not null,
     current_story integer references stories(story_id) not null,
-    chapter_id integer references chapters(chapter_id)
+    chapter_id integer references chapters(chapter_id) not null
 );
 
 insert into categories("category_id", "category_name") values(701, 'History');
@@ -67,10 +73,10 @@ insert into categories("category_id", "category_name") values(704, 'Adventure');
 insert into authors("author_id", "author_name", "author_email") values(501, 'Yaa Nuamah', 'edna@email.com');
 insert into authors("author_id", "author_name", "author_email") values(502, 'Isaac Agyen', 'edna@email.com');
 
-insert into stories("story_id", "story_title", "category_id", "story_photo", "author_id") values(101, 'My choice', 703, 'www.photo.com', 501);
-insert into stories("story_id", "story_title", "category_id", "story_photo", "author_id") values(102, 'Destiny', 702, 'www.photo.com', 501);
-insert into stories("story_id", "story_title", "category_id", "story_photo", "author_id") values(103, 'Strength', 701, 'www.photo.com', 502);
-insert into stories("story_id", "story_title", "category_id", "story_photo", "author_id") values(104, 'The Arms', 704, 'www.photo.com', 502);
+insert into stories("story_id", "story_title", "story_description", "category_id", "story_photo", "author_id") values(101, 'My choice', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', 703, 'www.photo.com', 501);
+insert into stories("story_id", "story_title", "story_description", "category_id", "story_photo", "author_id") values(102, 'Destiny', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', 703, 'www.photo.com', 501);
+insert into stories("story_id", "story_title", "story_description", "category_id", "story_photo", "author_id") values(103, 'Strength', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', 703, 'www.photo.com', 502);
+insert into stories("story_id", "story_title", "story_description", "category_id", "story_photo", "author_id") values(104, 'The Arms', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', 703, 'www.photo.com', 502);
 
 insert into chapters("chapter_id", "story_id", "chapter_title", "chapter_content") values(201, 101, 'There', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat');
 insert into chapters("chapter_id", "story_id", "chapter_title", "chapter_content") values(202, 101, 'Lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat');
@@ -82,5 +88,7 @@ insert into dependencies("dependency_id", "story_id", "from_chapter_id", "to_cha
 
 insert into choices("choice_id", "chapter_id", "details", "story_id") values(801, 202, 'next dependency', 101);
 
-insert into readers("reader_id", "reader_name", "reader_email", "chapter_id", "current_story") values(601, 'Shadrack Ankomahene', 'shad@gmail.com', 201, 101);
-insert into readers("reader_id", "reader_name", "reader_email", "chapter_id", "current_story") values(602, 'Sam Bartels', 'sam@gmail.com', 203, 102);
+insert into readers("reader_id", "reader_name", "reader_email") values(601, 'Shadrack Ankomahene', 'shad@gmail.com');
+insert into readers("reader_id", "reader_name", "reader_email") values(602, 'Sam Bartels', 'sam@me.io');
+
+insert into session_info("reader_id", "current_story", "chapter_id") values(601, 101, 201);
